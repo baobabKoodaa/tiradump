aebcc8296792c68d8a34ec420c635eb989a68039
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-01-12 18:21:59 +0200

    Testit

---
+++ vko2teht1/test/MainTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import fi.helsinki.cs.tmc.edutestutils.Points;
+import java.io.IOException;
+import java.util.Arrays;
+
+@Points("2.1")
+public class MainTest {
+    public void testi(String haku, int tulos) throws IOException {
+        int uusi = Main.count(haku);
+        assertTrue("Kun hakusana on " + haku + ", tiedostoja tulisi löytyä " +
+                   tulos + ", mutta metodisi palauttaa " + uusi + ".",
+                   uusi == tulos);
+    }
+    
+    @Test(timeout=1000)
+    public void esimerkit() throws IOException {
+        testi("Main.java", 1);
+        testi("Main", 5);
+    }
+
+    @Test(timeout=1000)
+    public void pituus6() throws IOException {
+        testi("rekursio", 1);
+        testi("level", 5);
+    }    
+    
+    
+}
